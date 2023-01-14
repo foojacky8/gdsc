41506e2c417c772b0f51259e2c2f2bce70c5e148
jfoo0016 <jfoo0016@student.monsah.edu>
2023-01-14 17:29:24 +0800

    feat: add flutter native splash screen

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -3,13 +3,16 @@ import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/get_routes.dart';
 import 'package:get/get.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
