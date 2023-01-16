4e58c241c89df888b25cc5afc119881cf7e08b04
jfoo0016 <jfoo0016@student.monsah.edu>
2023-01-16 23:25:28 +0800

    docs: add documention for User class variables

--- flutter_application/lib/authentication/models/user.dart
+++ flutter_application/lib/authentication/models/user.dart
@@ -1,7 +1,11 @@
 class User {
+  /// Email address of the user
   final String username;
+  /// Email address of the user
   final String email;
+  /// Smart meter number of the user
   final String? smartMeterNo;
+  /// Firebase authentication id of the user
   final String? id;
 
   User({
