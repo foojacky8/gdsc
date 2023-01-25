2336af4108344f729269d60813ee599fd351deb9
Etan01 <76555697+Etan01@users.noreply.github.com>
2023-01-25 18:56:50 +0800

    add home bottom bar file

--- flutter_application/lib/authentication/views/signup/my_signup_screen.dart
+++ flutter_application/lib/authentication/views/signup/my_signup_screen.dart
@@ -3,18 +3,34 @@ import 'package:flutter_application/authentication/controllers/signup_controller
 import 'package:flutter_application/splash_screen.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'package:get/get.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MySignupScreen extends StatelessWidget {
   const MySignupScreen({super.key});
+  Duration get loginTime => Duration(milliseconds: 2250);
 
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(SignupController());
 
     return FlutterLogin(
+      logo: AssetImage('assets/images/people2ppl.png'),
       title: 'Re:Energize',
       onLogin: controller.authUser,
       onSignup: controller.signupUser,
+
+        loginProviders: [
+          LoginProvider(
+            icon: FontAwesomeIcons.google,
+            label: 'Google',
+            callback: () async {
+              debugPrint('start google sign in');
+              await Future.delayed(loginTime);
+              debugPrint('stop google sign in');              
+              return null;
+            },
+          )
+        ],
       onSubmitAnimationCompleted: () {
         Navigator.of(context).pushReplacement(MaterialPageRoute(
           builder: (context) => const SplashScreen(),
--- flutter_application/lib/home/home_page.dart
+++ flutter_application/lib/home/home_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'home_bottom_bar.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(),
+      bottomNavigationBar: HomeBottomAppBar(),
+    );
   }
 }
